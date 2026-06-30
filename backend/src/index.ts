86cdaecbf4be1701d0de5c048693b60efa76fdd9
Markobanduka <banduka44@gmail.com>
2026-06-30 17:21:34 +0200

    rename chat.route

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,7 +1,8 @@
 import express from "express";
 import cors from "cors";
 import "dotenv/config";
-import chatRoutes from "./routes/chat.routes";
+import chatRoutes from "./routes/chat.route";
+import bookRoute from "./routes/book.route";
 
 import { getEnv } from "./lib/env";
 import keepAliveCron from "./lib/cron";
@@ -29,6 +30,7 @@ app.use(cors({
 }));
 
 app.use("/api/chat", chatRoutes);
+app.use("/api/sessions",bookRoute)
 
 
 app.get("/health", (_req, res) => {
